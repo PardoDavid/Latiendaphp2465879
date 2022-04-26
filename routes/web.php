1842e878b7ba2f400e34666cdd266bdf78cd12e6
Your Name <you@example.com>
2022-04-26 11:15:46 -0500

    arreglo paises

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,42 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('paises', function(){
+    $paises=[
+        "colombia"=>[
+            "capital"=> "Bogota",
+            "moneda"=>"peso",
+            "poblacion"=>51.6,
+            "ciudades"=>[
+                "Medellin",
+                "Cali",
+                "Barranquilla"
+            ]
+        ],
+        "peru"=>[
+            "capital"=> "Lima",
+            "moneda"=>"sol",
+            "poblacion"=>32.9,
+            "ciudades"=>[
+                "Cusco",
+                "Puno",
+                "Arequipa"
+            ]
+        ],
+        "paraguay"=>[
+            "capital"=> "Asuncion",
+            "moneda"=>"guarani",
+            "poblacion"=>7.1,
+            "ciudades"=>[
+                "Capiata",
+                "Villa Elisa",
+                "Filadelfia"
+            ]
+        ]
+    ];
+    
+    return view("paises")->with("paises",$paises);
+
+
+});
